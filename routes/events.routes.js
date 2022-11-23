bb1f319daff0b9795d65feff07de2eca08dd3ad4
Alariko0 <hector.mario86@gmail.com>
2022-11-23 16:01:05 +0100

    .

--- routes/events.routes.js
+++ routes/events.routes.js
@@ -1,15 +1,15 @@
 const express = require("express")
 const router = express.Router()
 
-router.get('./main', (req, res, next) => {
-    console.log('primera ruta')
+router.get('/main', (req, res, next) => {
+    res.render('auth/main')
 })
 
-router.get('./main2', (req, res, next) => {
+router.get('/main2', (req, res, next) => {
     console.log('segunda ruta')
 })
 
-router.get('./main3', (req, res, next) => {
+router.get('/main3', (req, res, next) => {
     console.log('tercera ruta')
 })
 
