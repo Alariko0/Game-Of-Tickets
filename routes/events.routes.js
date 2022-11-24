31331c8c09ca81c06e6f8f71608274311717d3e8
Alariko0 <hector.mario86@gmail.com>
2022-11-24 16:36:54 +0100

    res api

--- routes/events.routes.js
+++ routes/events.routes.js
@@ -1,15 +1,18 @@
-const { Router } = require("express")
+//const { Router } = require("express")
 const express = require("express")
 const router = express.Router()
 const ApiService = require("../services/api-service")
-const ticketApi = new ApiService
+const ticketApi = new ApiService()
 
 
 router.get('/main', (req, res, next) => {
-    res.render('auth/main', { ApiService })
-    // console.log(eventsList._embedded.events)
+    ticketApi.getAllEvents().then((events) => {
+        res.render('auth/main', { eventData: events._embedded.venues })
+        console.log(events)
+    })
 
 })
+
 
 router.get('/main2', (req, res, next) => {
     console.log('segunda ruta')
