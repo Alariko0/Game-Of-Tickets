5334254cf43bc1288dc52ee1ea57783474eb294e
Alariko0 <hector.mario86@gmail.com>
2022-11-24 22:21:43 +0100

    2nd api conect?

--- services/api-service.js
+++ services/api-service.js
@@ -11,6 +11,13 @@ class ApiService {
                 return response.json()
             })
     }
+
+    getId = () => {
+        fetch('https://app.ticketmaster.com/discovery/v2/attractions.json?apikey=GHIICd92WtEyOeF3wAVOc5OgPwPQd2kt')
+            .then((response) => {
+                return response.json()
+            })
+    }
 }
 
 module.exports = ApiService
