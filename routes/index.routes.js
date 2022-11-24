53c3ae98e9f0b0d86ee9a5235f4aa16180b8d26b
Larisa Mancebo <larisa.mancebo@gmail.com>
2022-11-24 13:51:33 +0100

    route change layout

--- routes/index.routes.js
+++ routes/index.routes.js
@@ -4,7 +4,8 @@ const userModel = require('../models/User.model');
 
 /* GET home page */
 router.get("/", (req, res, next) => {
-  res.render("index");
-});
+  res.render("index")
+})
+
 
 module.exports = router;
